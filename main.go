eced1bf3088b37c2d4657626cdaed17499b868a3
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-06-12 00:09:57 +0530

    chore: Add functions for generating sales report, employee performance report, and product inventory

--- main.go
+++ main.go
@@ -6,63 +6,118 @@ import (
 	"text/template"
 )
 
+type Header struct {
+	Company       string
+	FormatDate    string
+	FormatCurrency string
+}
+
 type MonthlySales struct {
-	Month  string
-	PreviousRevenue  int
+	Month          string
+	PreviousRevenue int
 	MonthlyRevenue int
-	Profit int
+	Profit         int
 }
 
 type Employee struct {
-	Position string
+	Position    string
 	Performance int
 }
 
-
-
-func ProductInventory() {
-
+type Product struct {
+	Product  string
+	Quantity int
+	Price    int
 }
 
-func EmployeePerformance(){
-	employees := map[string] Employee{
-		"John Doe": { "Manager", 60},
-		"Jane Smith" : {"Assistant Manager", 69},
-		"Tom White" : {"Sales Executive", 80},
-		"Jerry Black" : {"Sales Executive", 90},
-		"Harry Green" : {"Sales Executive", 70},
+func HeaderInfo() Header {
+	header := Header{"Evolza", "01/01/2021", "LKR"}
+	return header
+}
+
+func ProductInventory() {
+	products := []Product{
+		{"Laptop", 10, 100000},
+		{"Mobile", 20, 50000},
+		{"Tablet", 30, 30000},
+		{"Desktop", 40, 80000},
+		{"Printer", 50, 20000},
 	}
 
-	tmpl1, err:= template.ParseFiles("template2.txt")
+	header := HeaderInfo()
+	tmpl, err := template.ParseFiles("template3.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = tmpl.Execute(os.Stdout, struct {
+		Header   Header
+		Products []Product
+	}{
+		Header:   header,
+		Products: products,
+	})
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = tmpl1.Execute(os.Stdout, employees)
-	
+}
+
+func EmployeePerformance() {
+	employees := map[string]Employee{
+		"John Doe":    {"Manager", 60},
+		"Jane Smith":  {"Assistant Manager", 69},
+		"Tom White":   {"Sales Executive", 80},
+		"Jerry Black": {"Sales Executive", 90},
+		"Harry Green": {"Sales Executive", 70},
+	}
+
+	header := HeaderInfo()
+	tmpl, err := template.ParseFiles("template2.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = tmpl.Execute(os.Stdout, struct {
+		Header      Header
+		Employees   map[string]Employee
+	}{
+		Header:    header,
+		Employees: employees,
+	})
+
 	if err != nil {
 		fmt.Println(err)
 	}
-
 }
 
 func SalesReport() {
 	salesInfo := MonthlySales{"January", 900000, 1000000, 0}
-	profit := salesInfo.PreviousRevenue - salesInfo.MonthlyRevenue
-	salesInfo.Profit = profit
+	salesInfo.Profit = salesInfo.MonthlyRevenue - salesInfo.PreviousRevenue
 
+	header := HeaderInfo()
 	tmpl, err := template.ParseFiles("template.txt")
-	if err!= nil{
+	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	err = tmpl.Execute(os.Stdout, salesInfo)
-	if err!= nil{
+	err = tmpl.Execute(os.Stdout, struct {
+		Header     Header
+		MonthlySales MonthlySales
+	}{
+		Header:       header,
+		MonthlySales: salesInfo,
+	})
+
+	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func main() {
-
 	fmt.Print("Select the report type you need to generate: \n 1. Sales Report \n 2. Employee Performance \n 3. Product Inventory \n")
 	fmt.Print("Enter the number of the report type: ")
 	var reportType int
@@ -75,5 +130,7 @@ func main() {
 		EmployeePerformance()
 	case 3:
 		ProductInventory()
+	default:
+		fmt.Println("Invalid selection")
 	}
 }
